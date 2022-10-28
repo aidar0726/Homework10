2d3c2e0cdb347a12c23994f9356cda84f5bf8483
shakirov_a <Shakirov0726@gmail.com>
2022-10-28 18:42:10 +0600

    проверочный коммит (ДЗ 10)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("//задание 2");
+        String firstName = "Ivan";
+        String  middleName = "Ivanov";
+        String  lastName = "Ivanovich";
+
+        String fullName1 = firstName + " " + middleName + " " + lastName;
+        String fullName2 = fullName1.toUpperCase();
+        System.out.println("ФИО сотрудника — " + fullName1);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName2);
+
+        System.out.println("//задание 3");
+        String fullName = "Иванов Семён Семёнович";
+
+        String s2 = fullName.replace('ё', 'е');
+        System.out.println(s2);
     }
 }
